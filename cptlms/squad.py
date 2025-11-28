6ce0724b181f7ee5574c3fe2f30face876b6c804
mart-mihkel <mart.mihkel.aun@gmail.com>
2025-11-28 10:42:20 +0200

    squad metrics

--- cptlms/squad.py
+++ cptlms/squad.py
@@ -1,11 +1,23 @@
 import logging
-from typing import TypedDict, cast
+from collections import defaultdict
+from typing import Annotated, TypedDict, cast
 
+import evaluate
+import numpy as np
+from datasets.arrow_dataset import Dataset
 from datasets.dataset_dict import DatasetDict
 from datasets.load import load_dataset
+from torch._C import FloatTensor
+from tqdm.auto import tqdm
 from transformers import PreTrainedTokenizerFast
+from transformers.modeling_outputs import QuestionAnsweringModelOutput
 
 logger = logging.getLogger(__name__)
+
+
+class SQuADMetrics(TypedDict):
+    exact_match: float
+    f1: float
 
 
 class SQuADBatchAnswer(TypedDict):
@@ -21,8 +33,11 @@ class SQuADBatch(TypedDict):
 
 
 class SQuAD:
-    _max_len = 384
-    _stride = 128
+    _max_len_tokenizer = 384
+    _stride_tokenizer = 128
+
+    _n_best_eval = 20
+    _max_answer_len_eval = 30
 
     def __init__(self, tokenizer: PreTrainedTokenizerFast) -> None:
         logger.info("init SQuAD")
@@ -33,6 +48,7 @@ class SQuAD:
         self.data = data
         self.tokenizer = tokenizer
         self.tokenized = self._tokenize()
+        self.metric = evaluate.loading.load("squad")
 
     def _tokenize(self) -> DatasetDict:
         logger.info("tokenize SQuAD")
@@ -56,12 +72,12 @@ class SQuAD:
         inputs = self.tokenizer(
             questions,
             examples["context"],
-            stride=self._stride,
             padding="max_length",
-            max_length=self._max_len,
             truncation="only_second",
             return_offsets_mapping=True,
             return_overflowing_tokens=True,
+            stride=self._stride_tokenizer,
+            max_length=self._max_len_tokenizer,
         )
 
         answers = examples["answers"]
@@ -93,12 +109,12 @@ class SQuAD:
         inputs = self.tokenizer(
             questions,
             examples["context"],
-            stride=self._stride,
             padding="max_length",
-            max_length=self._max_len,
             truncation="only_second",
             return_offsets_mapping=True,
             return_overflowing_tokens=True,
+            stride=self._stride_tokenizer,
+            max_length=self._max_len_tokenizer,
         )
 
         sample_map: list[int] = inputs.pop("overflow_to_sample_mapping")
@@ -152,3 +168,102 @@ class SQuAD:
         end_pos = idx + 1
 
         return start_pos, end_pos
+
+    def compute_metrics(
+        self,
+        out: QuestionAnsweringModelOutput,
+        examples: Dataset,
+        tokenized_examples: Dataset,
+    ) -> SQuADMetrics:
+        predicted_answers = self._postprocess_predictions(
+            out=out,
+            examples=examples,
+            tokenized_examples=tokenized_examples,
+        )
+
+        theoretical_answers = [
+            {"id": ex["id"], "answers": ex["answers"]} for ex in examples
+        ]
+
+        metrics = self.metric.compute(
+            predictions=predicted_answers,
+            references=theoretical_answers,
+        )
+
+        return cast(SQuADMetrics, metrics)
+
+    def _postprocess_predictions(
+        self,
+        out: QuestionAnsweringModelOutput,
+        examples: Dataset,
+        tokenized_examples: Dataset,
+    ) -> list[dict[str, str | int]]:
+        start_logits = out.start_logits
+        end_logits = out.end_logits
+
+        assert start_logits is not None
+        assert end_logits is not None
+
+        example_to_features: dict[int, list[int]] = defaultdict(list)
+        for idx, feature in enumerate(tokenized_examples):
+            example_to_features[feature["example_id"]].append(idx)
+
+        predicted_answers = []
+        for example in tqdm(examples):
+            example_id = example["id"]
+            answers = self._extract_answers(
+                start_logits=start_logits,
+                end_logits=end_logits,
+                context=example["context"],
+                example_features=example_to_features[example_id],
+                offset_mapping=tokenized_examples["offset_mapping"],
+            )
+
+            if len(answers) > 0:
+                best_answer = max(answers, key=lambda x: x["logit_score"])
+                predicted_answers.append(
+                    {"id": example_id, "prediction_text": best_answer["text"]}
+                )
+            else:
+                predicted_answers.append({"id": example_id, "prediction_text": ""})
+
+        return predicted_answers
+
+    def _extract_answers(
+        self,
+        start_logits: Annotated[FloatTensor, "batch seq"],
+        end_logits: Annotated[FloatTensor, "batch seq"],
+        context: list[str],
+        example_features: list[int],
+        offset_mapping: list[list[tuple[int, int]]],
+    ) -> list[dict[str, str | int]]:
+        answers = []
+        for f_idx in example_features:
+            start_logit = start_logits[f_idx]
+            end_logit = end_logits[f_idx]
+            offsets = offset_mapping[f_idx]
+
+            start_indexes = np.argsort(start_logit)[
+                -1 : -self._n_best_eval - 1 : -1
+            ].tolist()
+
+            end_indexes = np.argsort(end_logit)[
+                -1 : -self._n_best_eval - 1 : -1
+            ].tolist()
+
+            for start_index in start_indexes:
+                for end_index in end_indexes:
+                    if offsets[start_index] is None or offsets[end_index] is None:
+                        continue
+
+                    if (
+                        end_index < start_index
+                        or end_index - start_index + 1 > self._max_answer_len_eval
+                    ):
+                        continue
+
+                    text = context[offsets[start_index][0] : offsets[end_index][1]]
+                    score = start_logit[start_index] + end_logit[end_index]
+                    answers.append({"text": text, "logit_score": score})
+
+        return answers
